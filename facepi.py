115d0440d665c3c3a42d7d1ee83bac3f3d6d76ed
KilnOfTheSecondFlame <pbauma@gmx.net>
2017-11-09 10:23:57 +0100

    Exported VideoStream to another Class

--- facepi.py
+++ facepi.py
@@ -1,36 +1,8 @@
 # Imports
 import cv2
-import sys
 import time
 import os
-
-# Prepare the Raspberry Pi Camera
-# https://www.pyimagesearch.com/2016/01/04/unifying-picamera-and-cv2-videocapture-into-a-single-class-with-opencv/
-class VideoStream:
-    def __init__(self, src=0, resolution=(320, 240),
-                 framerate=32):
-        # check to see if the picamera module should be used
-        # only import the picamera packages unless we are
-        # explicity told to do so -- this helps remove the
-        # requirement of `picamera[array]` from desktops or
-        # laptops that still want to use the `imutils` package
-        from PiVideoStream import PiVideoStream
-
-        # initialize the picamera stream and allow the camera
-        # sensor to warmup
-        self.stream = PiVideoStream(resolution=resolution, framerate=framerate)
-    def start(self):
-        # start a threaded video stream
-        return self.stream.start()
-    def update(self):
-        # grab the next frame from the stream
-        self.stream.update()
-    def read(self):
-        # return the current frame
-        return self.stream.read()
-    def stop(self):
-        # stop the thread and release any resources
-        self.stream.stop()
+import VideoStream
 
 vs = VideoStream().start()
 time.sleep(2.0)
