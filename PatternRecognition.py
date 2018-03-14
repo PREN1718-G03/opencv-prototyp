1c245320a90e25bd81ec43737cfcfe82f4c4f88c
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-03-14 14:56:13 +0100

    Prototyp PatternRecognition

--- PatternRecognition.py
+++ PatternRecognition.py
@@ -1,14 +1,10 @@
 import cv2
 import copy
-import time
-from VideoStream import VideoStream
-
-# Comment these two lines if you want to use the internal camera
-# cam  = VideoStream().start()
-# Let the camera sensor warm up
-# time.sleep(2.0)
+import numpy as np
 
 # Uncomment this line to use the internal camera
+from scipy.integrate._ivp.common import num_jac
+
 cam = cv2.VideoCapture(0)
 
 
@@ -21,6 +17,8 @@ while True:
     ret, frame = cam.read()
     # frame = cam.read()
 
+    template = cv2.imread('template.png', cv2.IMREAD_GRAYSCALE)
+
     cv2.imshow('Frame', frame)
 
     # Change the picture to grayscale
@@ -28,13 +26,51 @@ while True:
 
     cv2.imshow('Gray', gray)
 
+    width, height = gray.shape
+    normalized = np.zeros((width, height))
+
+    normalized = cv2.normalize(gray, normalized, 50, 255, cv2.NORM_MINMAX)
+
+    cv2.imshow('Normalized', normalized)
+
     # Calculate the binary threshold -> Everything lower than threshold turns black, everything over turns white
-    retthresh, threshold = cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
+    ret_thresh, threshold = cv2.threshold(normalized, 100, 255, cv2.THRESH_BINARY)
+
     # Show the calculated threshold
     cv2.imshow('Threshold', threshold)
 
-    # I don't want a reference to the frame object, I want a copy
-    cntImage = copy.copy(frame)
+    MAX_MATCHES = 500
+    MATCH_PERCENT = 0.01
+
+    orb = cv2.ORB_create(MAX_MATCHES)
+
+    template_keypoints, template_descriptors = orb.detectAndCompute(template, None)
+    image_keypoints, image_descriptors = orb.detectAndCompute(threshold, None)
+
+    image_matcher = cv2.DescriptorMatcher_create(cv2.DescriptorMatcher_BRUTEFORCE_HAMMING)
+
+    feature_matches = image_matcher.match(template_descriptors, image_descriptors, None)
+
+    feature_matches.sort(key=lambda x: x.distance, reverse=False)
+
+    num_good_matches = int(len(feature_matches) * MATCH_PERCENT)
+
+    feature_matches = feature_matches[:num_good_matches]
+
+    matches_frame = cv2.drawMatches(template, template_keypoints, threshold, image_keypoints, feature_matches, None)
+
+    match_points = np.zeros((len(feature_matches), 2), dtype=np.float32)
+
+    for i, match in enumerate(feature_matches):
+        match_points[i, :] = image_keypoints[match.trainIdx].pt
+
+    matches_threshold_frame = cv2.cvtColor(threshold, cv2.COLOR_BAYER_GR2RGB)
+
+    for point in match_points:
+        point_coordinates = (point[0], point[1])
+        cv2.drawMarker(matches_threshold_frame, point_coordinates, (255,0,0), 1, 6, 3)
+
+    cv2.imshow('MarkThresh', matches_threshold_frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
